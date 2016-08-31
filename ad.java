8bfd1d87c73ae7e4ab7b33a414373fb0cda42f40
Nandhini48 <nandhininandhuas@gmail.com>
2016-08-31 13:58:17 +0530

    Add files via upload

---
+++ ad.java
@@ -0,0 +1,15 @@
+public class Swap {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("enter ten numbers");
+        Scanner sc=new Scanner(System.in);
+        int a[]=new int[10];
+        int n,l=0;
+       for(int i=0;i<10;i++){
+           a[i]=sc.nextInt();
+           n=a[i];
+         if(n>l){
+               l=n;
+              }}System.out.println( "dvfbdjkg     "+l);
+        }
+      }
